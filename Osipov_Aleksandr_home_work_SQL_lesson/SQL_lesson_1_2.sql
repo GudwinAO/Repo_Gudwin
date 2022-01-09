58a00efea2c19e1fff7a9d7c61c3c0bde4625f79
Alex <Webmanagersupp@mail.ru>
2022-01-09 16:03:49 +0300

    SQL_lesson_1_2

--- Osipov_Aleksandr_home_work_SQL_lesson/SQL_lesson_1_2.sql
+++ Osipov_Aleksandr_home_work_SQL_lesson/SQL_lesson_1_2.sql
@@ -5,6 +5,13 @@ CREATE TABLE IF NOT EXISTS users (id INT(8), name VARCHAR(255));
 
 CREATE DATABASE IF NOT EXISTS Sample;
 
+CREATE DATABASE IF NOT EXISTS Test;
+
 # mysqldump -uroot -p Example > Example.sql
 
 # mysql -uroot -p Sample < Example.sql
+
+# mysqldump -uroot -p --where="true limit 100" mysql help_keyword > mysql.sql
+
+# mysql -uroot -p Test < mysql.sql вернет ошибку ERROR 3723 (HY000) at line 25: The table 'help_keyword' may not be created in the reserved tablespace 'mysql'
+
